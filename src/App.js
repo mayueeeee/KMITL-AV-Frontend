e28ee4e67dbecaa041d80a6cabfed74872ab89a6
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-11-05 01:32:16 +0700

    Add login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { inject, observer } from 'mobx-react';
+// import './App.css';
+import Login from './Pages/login'
 
+
+// @inject('routing')
+// @observer
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +23,11 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <Login/>
+
+
+
       </div>
     );
   }
---
+++ src/Pages/login.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Card, CardBody, Row, Col, Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+import bg from '../assets/backgrounds/2.jpg'
+
+const ContainerWarpper = styled.div`
+  background-image: url(${bg});
+  background-size: 100vw;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* filter: blur(5px);
+  position: absolute; */
+`
+const LoginBox = styled(Card)`
+  /* min-height:90vh;
+margin:20px 0 20px 0; */
+  /* filter: blur(0px) !important;
+  z-index: 100; */
+  max-width: 50%;
+`
+
+const LoginX = styled(Row)`
+  justify-content: center;
+`
+export default class Login extends Component {
+  render() {
+    return (
+      <ContainerWarpper>
+        <Container fluid>
+          <LoginX>
+            <Col xs={12} md={3}>
+              <Card>
+                <CardBody>
+                  <Row>
+                    <Col>
+                    <Form>
+                      <h2>เข้าสู่ระบบ</h2>
+                      <p>กรุณาเข้าสู่ระบบด้วยบัญชีสถาบัน</p>
+                      <FormGroup>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="KMITL Account" />
+                      </FormGroup>
+                      <FormGroup>
+                        <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+                      </FormGroup>
+                      <Button color="primary" block>เข้าสู่ระบบ</Button>
+                    </Form>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+          </LoginX>
+        </Container>
+      </ContainerWarpper>
+    )
+  }
+}
